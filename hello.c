7228a085f60c6a2c965176e2af3ad13d01a5fe29
unknown <samshinde5299@gmail.com>
2022-01-27 15:13:22 +0530

    adding hello.c file

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+  printf("\nHello world.!!");
+  int a = 10;
+  if(a > 5) {
+    printf("\nA is greater than 5");
+  } else {
+    printf("\nA is lesser than 5");
+  }
+}
+
